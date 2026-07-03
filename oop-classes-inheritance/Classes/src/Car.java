57abac8e80410a5f9196a3036031978991208399
Thomas <thomascode92@hotmail.com>
2026-07-03 21:44:39 +0200

    Setters, Object Creation, and OOP Practice

--- oop-classes-inheritance/Classes/src/Car.java
+++ oop-classes-inheritance/Classes/src/Car.java
@@ -10,20 +10,45 @@ public class Car {
         return make;
     }
 
+    public void setMake(String make) {
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> this.make = "Unknown";
+        }
+    }
+
     public String getModel() {
         return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public int getDoors() {
         return doors;
     }
 
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
     public boolean isConvertible() {
         return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 
     public void describeCar() {
